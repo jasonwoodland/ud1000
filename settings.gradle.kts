84cd2626584ea30195e11dd10635e5c5c4ecac72
Jason Woodland <me@jasonwoodland.com>
2023-09-19 13:51:40 +1000

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "ud1000"
+include(":app")
+ 
