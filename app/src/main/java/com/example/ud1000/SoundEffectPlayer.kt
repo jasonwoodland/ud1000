085e6d6a8ea0fb89ea1a1ee6fe80718e7836dc98
Jason Woodland <me@jasonwoodland.com>
2023-09-24 23:52:41 +1000

    audio: sound effects

---
+++ app/src/main/java/com/example/ud1000/SoundEffectPlayer.kt
@@ -0,0 +1,37 @@
+package com.example.ud1000
+
+import android.content.Context
+import android.media.MediaPlayer
+
+class SoundEffectPlayer(context: Context, resId: Int) {
+
+    private var mediaPlayer: MediaPlayer? = MediaPlayer.create(context, resId)
+
+    fun play() {
+        mediaPlayer?.let {
+            if (!it.isPlaying) {
+                it.start()
+            }
+        }
+    }
+
+    fun stop() {
+        mediaPlayer?.let {
+            if (it.isPlaying) {
+                it.stop()
+                it.prepare() // Prepare it for future playback
+            }
+        }
+    }
+
+    fun release() {
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+    companion object {
+        fun newInstance(context: Context, resId: Int): SoundEffectPlayer {
+            return SoundEffectPlayer(context, resId)
+        }
+    }
+}
